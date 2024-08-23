8cfcd03da6d9b89e96aff0b72339406dce077e3f
khoa <user.iamjasper@gmail.com>
2024-08-23 15:38:04 +0700

    add homepage

---
+++ components/ProductCard.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import { Heart } from 'lucide-react'
+
+const ProductCard = ({ product }: { product: ProductType }) => {
+    return (
+        <Link href={`/products/${product._id}`} className="w-[220px] flex flex-col gap-2">
+            <Image
+                src={product.media[0]}
+                alt="product"
+                width={250}
+                height={300}
+                className="h-[250px] rounded-lg object-cover"
+            />
+            <div>
+                <p className="text-base-bold">{product.title}</p>
+                <div className="text-small-medium text-grey-2">{product.category}</div>
+            </div>
+            <div className="flex justify-between items-center">
+                <p className="text-body-bold">${product.price}</p>
+                <button>
+                    <Heart />
+                </button>
+            </div>
+        </Link>
+    )
+}
+
+export default ProductCard
